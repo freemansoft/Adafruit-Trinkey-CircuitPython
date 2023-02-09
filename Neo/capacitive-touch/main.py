5b8909ecc61b4c5a8339aea7e0ca665e8bfcfb9d
Joe Freeman <joe@freemansoft.com>
2023-02-09 07:58:58 -0500

    3 led patterns instead of two because it sprettier

--- Neo/capacitive-touch/main.py
+++ Neo/capacitive-touch/main.py
@@ -52,8 +52,9 @@ while True:
         # Other HID keyboard possible actions
         # keyboard_layout.write("Hello World!\n")  # Then send string.
 
+    # This is totally Brute Force and Ignorance (BFI)
     # blanks and resets color in last cycle_blank_length_tics through the cycle
-    # two different led configurations - each 1/2 of non blank cycle
+    # three different led configurations - each 1/3 of non blank cycle
     # increments but rolls over the current tics if at end of cycle
     current_tics = (current_tics + 1) % cycle_length_tics
     # blank at end of the cycle so we always show color on change
@@ -63,13 +64,17 @@ while True:
         pixels[0] = current_color
         pixels[2] = current_color
         pixels.show()
-    elif current_tics == (cycle_length_tics - cycle_blank_length_tics) // 2:
+    elif current_tics == (cycle_length_tics - cycle_blank_length_tics) // 3:
         pixels[1] = current_color
         pixels[3] = current_color
         pixels.show()
+    elif current_tics == (cycle_length_tics - cycle_blank_length_tics) * 2 // 3:
+        pixels[0] = (0, 0, 0)
+        pixels[2] = (0, 0, 0)
+        pixels.show()
     elif current_tics > cycle_length_tics - cycle_blank_length_tics:
-        # show blank
-        # reset to default after blanking window
+        # turn off all the LEDs (blank)
+        # reset to default for after blanking window
         # continually show blank until after blanking_window then restore color
         current_color = (0, 0, 0)
         pixels.fill(current_color)
